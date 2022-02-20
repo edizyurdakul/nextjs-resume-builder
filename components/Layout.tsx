ab247cb2047724b9f56778cd6afcba80f012fd36
Ediz <53665610+edizyurdakul@users.noreply.github.com>
2022-02-20 18:05:31 +0200

    Style banner, fixed

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,10 +7,20 @@ interface LayoutProps {
 function Layout({ children }: LayoutProps) {
   return (
     <>
-      <Center sx={{ background: "#5F3DC4", padding: "0.5rem 0.5rem" }}>
+      <Center
+        sx={{
+          background: "#5F3DC4",
+          padding: "0.5rem 0.5rem",
+          position: "fixed",
+          top: "0",
+          left: "0",
+          right: "0",
+          zIndex: "1",
+        }}
+      >
         <Text size={"xs"}>Currently under development</Text>
       </Center>
-      <Container mt={32}>{children}</Container>
+      <Container mt={64}>{children}</Container>
     </>
   );
 }
